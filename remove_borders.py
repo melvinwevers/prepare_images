0a6709f2e8307f0e0d7c33676a26019cb4fe9f4d
Melvin Wevers <melvinwevers@gmail.com>
2021-02-24 17:57:52 +0100

    added thresholding to fix segmenation errors

--- remove_borders.py
+++ remove_borders.py
@@ -4,19 +4,44 @@ import os
 import progressbar
 
 
-def crop_biggest(imgPath, outPath, threshold = 50):
+def crop_biggest(imgPath, outPath, threshold = 50, resize=True):
+    w = 0
+    h = 0
+    
     img = cv2.imread(imgPath, 1)
     file_name = os.path.basename(imgPath)[:-4]
     rgbimg = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
     image_gray = cv2.cvtColor(rgbimg, cv2.COLOR_BGR2GRAY)
-    _,threshold = cv2.threshold(image_gray, threshold, 255,0)
+
+    while w < 3000 or h < 2300:
+        _, threshold_ = cv2.threshold(image_gray, threshold, 255,0)
+        contours, hierarchy = cv2.findContours(threshold_, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours]
+
+        biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
+        x, y, w, h = cv2.boundingRect(biggest_contour)
+
+        threshold -= 1 # lower threshold if images is too small
+
+    if w > 3600 or h > 2500:
+        # if images are still too large add their filenames to exported list
+        mistakes.append(file_name)
+        pass
     
-    contours, hierarchy = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours]
-    
-    biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
-    x, y, w, h = cv2.boundingRect(biggest_contour)
     roi = img[y  :y + h, x : x + w ]
+
+    if resize:
+        scale_percent = 25
+
+        width = int(roi.shape[1] * scale_percent / 100)
+        height = int(roi.shape[0] * scale_percent / 100)
+        
+        # dsize
+        resized = (width, height)
+
+        # resize image
+        roi = cv2.resize(roi, resized)
+    
     export_file_name = (f"{file_name}_crop.jpg")
     cv2.imwrite(os.path.join(outPath, export_file_name), roi)
 
@@ -32,6 +57,8 @@ if __name__ == '__main__':
     output_path = args.output_path if args.output_path else args.input_path
     threshold = args.threshold
 
+    mistakes = []
+
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
@@ -41,6 +68,10 @@ if __name__ == '__main__':
             imgPath = os.path.join(input_path, image)
             crop_biggest(imgPath, output_path)
             bar.update(i)
+
+    with open('mistakes.txt', 'w') as f:
+        for item in mistakes:
+            f.write("%s\n" % item)
         
 
     
